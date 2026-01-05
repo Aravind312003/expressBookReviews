c122f02f6b9f9ec4d60133b9515c4357fc56c56a
Aravind312003 <aravinddeepak03@gmail.com>
2026-01-05 03:02:02 -0500

    Task 11: Get book details by ISBN using Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,3 +23,19 @@ public_users.get('/', function (req, res) {
             res.status(500).json({ message: error });
         });
 });
+
+// Task 11: Get book details based on ISBN using Promises
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const getBook = new Promise((resolve, reject) => {
+        if (books[isbn]) {
+            resolve(books[isbn]);
+        } else {
+            reject("Book not found");
+        }
+    });
+
+    getBook
+        .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
+        .catch((err) => res.status(404).json({ message: err }));
+});
